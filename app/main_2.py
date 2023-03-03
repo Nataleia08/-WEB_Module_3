0d5dd2e343ff8c443ce890300f1f50349d3ad0df
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-03 20:10:42 +0200

    part 2 ver 1

--- app/main_2.py
+++ app/main_2.py
@@ -1,6 +1,6 @@
 from multiprocessing import cpu_count
 from time import time
-from multiprocessing import Process, Pool
+from multiprocessing import Process, Pool, Manager
 
 def factorize(number: list) -> list:
     result_all = []
@@ -25,15 +25,34 @@ if __name__ == "__main__":
 
 
     prs = []
-    timers = []
+    timers_start = []
     for i in [128, 255, 99999, 10651060]:
         timer = time()
-        timers.append(timer)
+        timers_start.append(timer)
         pr = Process(name= f"{i} number process", target= factorize, args= ([i],))
         pr.start()
         prs.append(pr)
-    [pr.join() for pr in prs]
-    [print(f"{pr.name} {round(time() - timer, 4)} second") for timer in timers]
+        # pr.join()
+    # [pr.join() for pr in prs]
+    timers_end = []
+    for pr in prs:
+        pr.join()
+        timer = time()
+        timers_end.append(timer)
+    # for tm1, tm2 in timers_start, timers_end:
+    #     print(f"{pr.name} {round(tm2 - tm1, 4)} second")
+    # [print(f"{pr.name} {round(time() - tm, 4)} second") for tm in timers]
 
     with Pool(processes=4) as pool:
-        pool.map(factorize, [[128], [255], [99999], [10651060]])
+        print(pool.map(factorize, [[128], [255], [99999], [10651060]]))
+
+    with Manager() as manager:
+        m = manager.dict()
+        processes = []
+        for i in [128, 255, 99999, 10651060]:
+            pr = Process(target=factorize, args=([i], m))
+            pr.start()
+            processes.append(pr)
+
+        [pr.join() for pr in processes]
+        print(m)
